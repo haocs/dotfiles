4f5017d66c416b0eeb4af5fc81eae438fdbcb4a7
haoc <haoc@localhost.localdomain>
2015-07-21 13:50:41 -0500

    add .gitconfig

--- git/git_install.sh
+++ git/git_install.sh
@@ -3,6 +3,10 @@
 if [ -f ~/.gitconfig ]; then
 	echo "Found old .gitconfig, will archive ~> .gitconfig_bk"
 	mv ~/.gitconfig ~/.gitconfig_bk
+elif [ -L ~/.gitconfig ]; then
+	echo "Found old .gitconfig ln, will remove"
+	rm ~/.gitconfig
 fi
 
-mv .gitconfig ~/ 
+# create a soft link
+ln -s gitconfig /home/haoc/.gitconfig
