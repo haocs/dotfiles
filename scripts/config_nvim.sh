8442052edfc98169b2db2d6598824549f0be2a50
Hao Chen <hach@microsoft.com>
2016-04-16 13:52:16 -0700

    ...

---
+++ scripts/config_nvim.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+INSTALL_TO="$HOME/.config/nvim"
+S_DIR=$(readlink -f "$(pwd)/../nvim")
+
+install_vimrc () {
+    if [ -e "$INSTALL_TO/init.vim" ];
+    then
+	    echo "init.vim exists will make it init.vim_bk"
+	    mv "$INSTALL_TO/init.vim" "$INSTALL_TO/init.vim_vk"
+    fi
+    if [ ! -d "$INSTALL_TO" ];
+    then
+	    echo "create new folder"
+	    mkdir -p "$INSTALL_TO"
+    fi
+
+    ln -s "$S_DIR/init.vim" `readlink -f "$INSTALL_TO/init.vim"`
+
+	echo "alias vim='nvim'" | tee -a "$HOME/.bashrc"
+    echo "Installed and configured nvim."
+}
+
+install_vimrc
