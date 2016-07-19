485c6a6bc4604f3ee47ed7019d17013a1e415c02
Hao Chen <hach@microsoft.com>
2016-07-18 17:18:32 -0700

    Updated nodejs installation script for deb

--- scripts/setup_deb.sh
+++ scripts/setup_deb.sh
@@ -6,6 +6,7 @@ exists() {
 
 init() {
 	if [ ! -d "$HOME/.local/bin" ]; then mkdir -p "$HOME/.local/bin"; fi
+	sudo apt-get install -y build-essential
 }
 
 install_go() {
@@ -33,11 +34,8 @@ install_py3() {
 }
 
 install_nodejs() {
-	NODEJS_APT_ROOT="node_4.x"
-	NODEJS_VERSION="4.2.4"
-    curl https://deb.nodesource.com/${NODEJS_APT_ROOT}/pool/main/n/nodejs/nodejs_${NODEJS_VERSION}-1nodesource1~jessie1_amd64.deb > node.deb && \
-      dpkg -i node.deb && \
-      rm node.deb && \
+	curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+	sudo apt-get install -y nodejs
 }
 
 install_ruby() {
